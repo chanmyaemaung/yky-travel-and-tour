3db0b5ca5096b6925db0bd326c40d3f022edbae8
Chan Myae Maung <maungmaungyoker.mmy@gmail.com>
2019-07-07 18:26:50 +0630

    switch db to local

--- configs/keys.js
+++ configs/keys.js
@@ -1,5 +1,5 @@
-dbPassword = 'mongodb://chanlay:chanlay123@yaykyiyar-shard-00-01-ajgvf.azure.mongodb.net:27017,yaykyiyar-shard-00-01-ajgvf.azure.mongodb.net:27017,yaykyiyar-shard-00-01-ajgvf.azure.mongodb.net:27017/yky-blog?ssl=true&replicaSet=yaykyiyar-shard-0&authSource=admin&retryWrites=true'
+dbPassword = 'mongodb://127.0.0.1:27017/yky-blog-api'
 
 module.exports = {
     mongoURI: dbPassword
-};
+}
